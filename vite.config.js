5a63213d2eb4888529be853af1672ac729203b89
jinqiang.mao <mjq1004@163.com>
2025-01-02 00:22:11 +0800

    路径提交

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/ittool/dist/',
+  base: '/ittool/',
   root: './',
   build: {
-    outDir: 'dist'
+    outDir: './dist'
   }
 }) 
